ef07779449e77dd2bd1bd49e6dcbf3a366b7c2bd
David Stephens <stephens.a.david@gmail.com>
2019-11-14 10:34:24 -0600

    updated depenedencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import $ from 'jquery';
-import {sayHelloAgain} from "./say-hello";
+// import {sayHelloAgain} from "./say-hello";
+//
+// const sayHello = () => console.log('hello');
+//
+// sayHello();
+//
+// console.log(sayHelloAgain());
 
-const sayHello = () => console.log('hello');
-
-sayHello();
-
-console.log(sayHelloAgain());
-
-$('body').css('background-color', 'lavender').html('<h1>Hello from JS!</h1>');
+$('body').css('background-color', 'burlywood').html('<h1>Hello from JS!</h1>').css('color', "firebrick");
